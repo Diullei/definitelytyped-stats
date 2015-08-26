8b810af0046d6621fa1f5200377550f8f30fce57
Diullei <diullei@gmail.com>
2015-08-26 12:12:26 -0300

    split script

--- run.sh
+++ run.sh
@@ -14,16 +14,4 @@ done
 
 node dist/gen-definition-index.js
 
-git stash
-git checkout stats
-rm definition-index.json
-git add -u
-git stash apply
-git add .
-
-NOW=$(date +"%m-%d-%Y %H:%M")
-
-git commit -m "update $NOW"
-git checkout master
-
-git push
+sh try-publish.sh
